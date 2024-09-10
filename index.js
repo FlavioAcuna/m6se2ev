ac4173f66f0270fd5430b0b25b35e5c3215a7127
FlavioAcuña <42254195+FlavioAcuna@users.noreply.github.com>
2024-09-10 10:49:10 -0300

    modified:   index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import _ from "lodash";
 import { animales, autos } from "./datos.js";
 
-var uniq_Car = _.uniq(autos);
-console.log(uniq_Car);
+var uniq_car = _.uniq(autos);
+console.log(uniq_car);
+
+var first_wild_animal = _.find(animales, { tipo: "salvaje" });
+console.log(first_wild_animal);
+
+var wild_animals = _.filter(animales, { tipo: "salvaje" });
+console.log(wild_animals);
